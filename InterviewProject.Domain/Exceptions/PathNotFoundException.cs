f7a85269ab2b86bd1d5a3802d2f5876bd4b97d2b
mszczykowski <67839881+mszczykowski@users.noreply.github.com>
2022-04-25 23:57:47 +0200

    bugfix

--- InterviewProject.Domain/Exceptions/PathNotFoundException.cs
+++ InterviewProject.Domain/Exceptions/PathNotFoundException.cs
@@ -8,7 +8,7 @@ namespace InterviewProject.Domain.Exceptions
 {
     public class PathNotFoundException : Exception
     {
-        public PathNotFoundException(string start, string destiation) : base($"Path beetween \"{start}\" and \"{destiation}\" cannot be found.")
+        public PathNotFoundException(string start, string destiation) : base($"Path beetween {start} and {destiation} cannot be found.")
         {
 
         }
